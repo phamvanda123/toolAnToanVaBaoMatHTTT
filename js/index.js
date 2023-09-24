b296f809cabbeacc57e5acf4824d5dd6df32f6e7
phamvanda123 <114846809+phamvanda123@users.noreply.github.com>
2023-09-24 21:20:55 +0700

    commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
+function load() {
+    Swal.fire({
+        title: 'Trang web do Phạm Văn Dã Phát triển hãy nhập vandadeptrai để tiếp tục',
+        input: 'text',
+        inputPlaceholder: 'Nhập thông tin ở đây...',
+        showCancelButton: true,
+        confirmButtonText: 'OK',
+        cancelButtonText: 'Hủy bỏ'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            const userInput = result.value;
+            if (userInput !== '') {
+                if (userInput == "vandadeptrai") {
+                    active()
+                    Swal.fire('active thành công')
+                } else {
+                    Swal.fire('Sai Roài')
+                }
+            } else {
+                Swal.fire('Không nhập đừng hòng xài');
+            }
+        } else {
+            Swal.fire('Bạn đã hủy bỏ.');
+        }
+    });
+
+
+}
+
+function active() {
+    const overlay = document.querySelector('.active');
+    overlay.style.display = 'none';
+}
+
 function calculate() {
     var input1 = document.getElementById('input1').value;
     var input2 = document.getElementById('input2').value;
@@ -75,15 +109,47 @@ function EndCode() {
     var chuoiDauTien = chuyenChuoiThanhObject(txtEndcodeValue.toUpperCase());
     tinhmod.innerHTML = `Công thức: e<sub>K</sub>(x) =  ${khoaAValue}x + ${khoaBValue} mod ${zEndCodeValue}`;
     var chuoiEndcode = "";
-    for (var i = 0; i < chuoiDauTien.length; i++) {
-        var item = chuoiDauTien[i];
-        console.log();
+    var chuoikq = []
+    for (let i = 0; i < chuoiDauTien.length; i++) {
+        let item = chuoiDauTien[i];
         tinhmod.innerHTML += `<br> e<sub>K</sub>(${item.so}) =  ${khoaAValue}x${item.so} + ${khoaBValue} mod ${zEndCodeValue} = ${mod((parseInt(khoaAValue) * item.so + parseInt(khoaBValue)),zEndCodeValue)}`
         chuoiEndcode += String.fromCharCode(mod((parseInt(khoaAValue) * item.so + parseInt(khoaBValue)), zEndCodeValue) + 65)
         row1.innerHTML += "<td>" + item.ma + "</td>";
-        row2.innerHTML += "<td>" + item.so + "</td>"
+        row2.innerHTML += "<td>" + item.ma + "</td>";
+        chuoikq.push(mod((parseInt(khoaAValue) * item.so + parseInt(khoaBValue)), zEndCodeValue))
     }
+    tinhmod.innerHTML += createHTMLTableFromArray(chuyenSoThanhObject(chuoikq), 2)
+
+
     document.getElementById("outputEndcode").value = chuoiEndcode;
+}
+
+function DeCode() {
+    var txtDeCode = document.getElementById("txt_decode").value;
+    var khoaAValue = document.getElementById("KhoaAdecode").value;
+    var khoaBValue = document.getElementById("KhoaBdecode").value;
+    var zDeCode = document.getElementById("ZdeCode").value;
+    var row1 = document.getElementById("Chu1");
+    var row2 = document.getElementById("So1");
+    var tinhmod = document.getElementById("pheptinhmod1")
+    var chuoiDauTien = chuyenChuoiThanhObject(txtDeCode.toUpperCase());
+    tinhmod.innerHTML = `Khả nghịch của ${khoaAValue} là: ${KhaNgich(khoaAValue,26)} <br>`
+    tinhmod.innerHTML += `Công thức: e<sub>K</sub>(x) =  ${KhaNgich(khoaAValue,26)} x(y - ${khoaBValue}) mod ${zDeCode}`;
+    var ChuoiDeCode = "";
+    var chuoikq = []
+    for (let i = 0; i < chuoiDauTien.length; i++) {
+        let item = chuoiDauTien[i];
+        tinhmod.innerHTML += `<br> e<sub>K</sub>(${item.so}) =  ${KhaNgich(khoaAValue,26)} x(${item.so} - ${khoaBValue}) mod ${zDeCode} = ${mod((parseInt(KhaNgich(khoaAValue,26)) * ((item.so - parseInt(khoaBValue)))),zDeCode)}`
+        ChuoiDeCode += String.fromCharCode(mod((parseInt(KhaNgich(khoaAValue, 26)) * (item.so - parseInt(khoaBValue))), zDeCode) + 65)
+        console.log(mod(parseInt(KhaNgich(khoaAValue, 26)) * (item.so - parseInt(khoaBValue)), 26))
+        row1.innerHTML += "<td>" + item.ma + "</td>";
+        row2.innerHTML += "<td>" + item.so + "</td>";
+        chuoikq.push(mod((parseInt(KhaNgich(khoaAValue, 26)) * (item.so - parseInt(khoaBValue))), zDeCode))
+    }
+    tinhmod.innerHTML += createHTMLTableFromArray(chuyenSoThanhObject(chuoikq), 2)
+
+
+    document.getElementById("outputdecode").value = ChuoiDeCode;
 }
 
 function chuyenChuoiThanhObject(chuoi) {
@@ -102,4 +168,46 @@ function chuyenChuoiThanhObject(chuoi) {
     }
 
     return ketQua;
+}
+
+function chuyenSoThanhObject(ChuoiSo) {
+    var ketQua = [];
+    for (var i = 0; i < ChuoiSo.length; i++) {
+        var kyTu = String.fromCharCode(ChuoiSo[i] + 65);
+        var maASCII = ChuoiSo[i];
+
+        var kyTuObject = {
+            ma: kyTu,
+            so: maASCII
+        };
+        ketQua.push(kyTuObject);
+    }
+    console.log(ketQua)
+    return ketQua;
+}
+
+function createHTMLTableFromArray(arr, kieu) {
+    if (!Array.isArray(arr) || arr.length === 0) {
+        return ''; // Trả về chuỗi trống nếu mảng không hợp lệ hoặc rỗng
+    }
+    var html = "<table class='table table-bordered'>"
+    var row1 = "<tr> <td>Chữ</td>"
+    var row2 = "<tr> <td>Số </td>"
+    for (let i = 0; i < arr.length; i++) {
+        let item = arr[i];
+        row1 += "<td>" + item.ma + "</td>";
+        row2 += "<td>" + item.so + "</td>"
+    }
+    row1 += "</td>"
+    row2 += "</td>"
+    if (kieu === 1) {
+        html += row1;
+        html += row2;
+    } else {
+        html += row2;
+        html += row1;
+    }
+
+    html += "</table>"
+    return html;
 }
